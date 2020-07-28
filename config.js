2c91bf69f1af4d8ae7ecd309cd18239a5626895c
Shruti Bhatt <shruti2406m@gmail.com>
2020-07-28 21:08:48 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '729252844303372',
+        clientSecret: '89e6370d9f00fc0d752e6bb4553b66e2'
+    }
 }
