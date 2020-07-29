9172ac82b348d16a95a692d748cb8247961f3c6c
Shruti Bhatt <shruti2406m@gmail.com>
2020-07-29 18:56:22 +0530

    Integrating React Client and Server

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
 var uploadRouter = require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
+var commentRouter = require('./routes/commentRouter');
 
 const mongoose = require('mongoose');
 
@@ -61,6 +63,8 @@ app.use('/dishes',dishRouter);
 app.use('/leaders',leaderRouter);
 app.use('/promotions',promoRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites', favoriteRouter);
+app.use('/comments',commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
